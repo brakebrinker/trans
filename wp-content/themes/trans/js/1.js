df90359f30aa67b553dabf58ffef85c8ce30f636
zumtum <drummen11@gmail.com>
2017-07-31 11:47:06 +0300

    Transingenering WordPress, 31.07 fist commit

---
+++ wp-content/themes/trans/js/1.js
@@ -0,0 +1,14 @@
+﻿//tabs
+$('#descr-tab').click(function(){
+	$(this).addClass('active');
+	$('#tth-tab').removeClass('active');
+	$('.description').css('display', 'block');
+	$('.tth').css('display', 'none');
+	
+});
+$('#tth-tab').click(function(){
+	$(this).addClass('active');
+	$('#descr-tab').removeClass('active');
+	$('.tth').css('display', 'block');
+	$('.description').css('display', 'none');
+});
